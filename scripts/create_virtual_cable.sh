ae03252a668c4b1b97869a30119ba70406b2aebf
Henry <henryweaponsguy@proton.me>
2025-11-13 01:12:53 +0100

    Add files via upload

--- scripts/create_virtual_cable.sh
+++ scripts/create_virtual_cable.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-virtual_sink_name="virtual_sink"
-virtual_mic_name="virtual_mic"
+virtual_speaker_name="virtual_speaker"
+virtual_microphone_name="virtual_microphone"
 virtual_cable_modules="/tmp/virtual_cable_modules"
 
 if [ ! -f "$virtual_cable_modules" ]; then
     touch "$virtual_cable_modules"
 
-    # Create a virtual sink
+    # Create a virtual speaker
     pactl load-module module-null-sink \
-    sink_name="$virtual_sink_name" \
-    sink_properties=device.description="$virtual_sink_name" \
+    sink_name="$virtual_speaker_name" \
+    sink_properties=device.description="$virtual_speaker_name" \
     >> "$virtual_cable_modules"
 
-    # Create a virtual mic
+    # Create a virtual microphone
     pactl load-module module-null-sink \
-    sink_name="$virtual_mic_name" \
-    sink_properties=device.description="$virtual_mic_name" \
+    sink_name="$virtual_microphone_name" \
+    sink_properties=device.description="$virtual_microphone_name" \
     media.class=Audio/Source/Virtual \
     channel_map=front-left,front-right \
     >> "$virtual_cable_modules"
 
-    # Link the virtual sink and mic
-    pw-link "$virtual_sink_name":monitor_FL "$virtual_mic_name":input_FL
-    pw-link "$virtual_sink_name":monitor_FR "$virtual_mic_name":input_FR
+    # Link the virtual speaker and microphone
+    pw-link "$virtual_speaker_name":monitor_FL "$virtual_microphone_name":input_FL
+    pw-link "$virtual_speaker_name":monitor_FR "$virtual_microphone_name":input_FR
 else
-    # Remove the virtual sink and mic
+    # Remove the virtual speaker and microphone
     while IFS= read -r line; do
         pactl unload-module "$line"
     done < "$virtual_cable_modules"
